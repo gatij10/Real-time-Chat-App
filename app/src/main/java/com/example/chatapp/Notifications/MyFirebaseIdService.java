cd85387a622e1b025be21c8da0a9ffe8a1aaea01
gatij10 <Your-Email>
2020-04-16 23:17:38 +0530

    Added Notification feature.

--- app/src/main/java/com/example/chatapp/Notifications/MyFirebaseIdService.java
+++ app/src/main/java/com/example/chatapp/Notifications/MyFirebaseIdService.java
@@ -5,27 +5,26 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessagingService;
 
-import androidx.annotation.NonNull;
-
 public class MyFirebaseIdService extends FirebaseMessagingService {
-    @Override
-    public void onNewToken(@NonNull String s) {
-        super.onNewToken(s);
-        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
 
-        String refreshToken = FirebaseInstanceId.getInstance().getToken();
+
+    public void onNewToken(String refreshToken) {
+        super.onNewToken(refreshToken);
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
+        String refreshedToken = refreshToken;
         if (firebaseUser != null){
             updateToken(refreshToken);
         }
     }
 
     private void updateToken(String refreshToken) {
-        FirebaseUser firebaseUser= FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Tokens");
-        Token token= new Token(refreshToken);
+        Token token = new Token(refreshToken);
         reference.child(firebaseUser.getUid()).setValue(token);
     }
 }
